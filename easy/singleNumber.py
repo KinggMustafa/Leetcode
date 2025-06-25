87725d868ae633f3a85eff13e39a32b2b8284653
Abraham Mustafa <abrahammustafa2015@gmail.com>
2025-06-25 02:28:24 -0400

    adding SingleNumber

---
+++ easy/singleNumber.py
@@ -0,0 +1,24 @@
+class Solution:
+    def singleNumber(self, nums) -> int:
+        #use xor operand to cancel out dupllicate numbers in the list
+        #xor returns 1 if the bits are not the same
+        #1 xor 3 equals 2 bc only 1 bit gets canceled out 
+        operand = 0 #anything xor 0 is the number itself
+        for num in nums:
+            operand = num ^ operand
+        return operand
+    
+if __name__ == '__main__':
+    #easiest testcase to visualize
+    list1 = [2,2,1]
+    #xor 2,2 = 0 xor 1 is just 1
+    test1 = Solution().singleNumber(list1)
+    if test1:
+        print(f' single number: {test1}')
+
+    list2 = [4,1,2,1,2]
+    test2 = Solution().singleNumber(list2)
+    if test2:
+        print(f'single Number: {test2}')
+
+    
